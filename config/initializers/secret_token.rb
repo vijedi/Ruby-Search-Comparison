6a245036dde73ef8fd1dad337d121b8dd6ce0fda
Tejus Parikh <tejus@vijedi.net>
2010-11-03 21:30:40 -0400

    initalizing repository. adding in data creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchComparisions::Application.config.secret_token = '910c8c760e648ca559d7d95afd89e8b8590d1992fa331c5aad627cd180ea6f86946a76298320dcbb5b7451702aec567d518de3ff450cf291a0315d577b768426'
---
+++ db/migrate/20101104004948_create_basic_model.rb
@@ -0,0 +1,26 @@
+class CreateBasicModel < ActiveRecord::Migration
+    def self.up
+        create_table :users do |t|
+            t.string :first_name
+            t.string :last_name
+            t.text   :data
+        end
+
+        names = []
+        File.open("data/dist.male.first").each_line do |line|
+            names << line.split(/\s+/)[0]
+        end
+        
+        file = File.open("data/down_and_out_in_the_magic_kingdom.txt")
+        
+        while(text = file.read(2048)) do 
+            User.create(:first_name => names[(names.length * rand).to_i], 
+                    :last_name => names[(names.length * rand).to_i],
+                    :data => text)
+        end
+    end
+
+    def self.down
+      drop_table :users
+    end
+end
